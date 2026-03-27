f39833d1d83333b43054e02828aa5acbfc992d5f
Abegail Isidro <chabbyabe@gmail.com>
2026-03-27 19:21:35 +0100

    Create ledger migrations

--- ledger/migrations/0002_initial.py
+++ ledger/migrations/0002_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('ledger', '0001_initial'),
-        ('users', '0002_account'),
+        ('users', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 

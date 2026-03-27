96603b86b366a37ce8223d1cd7ac2edbe009ead0
Abegail Isidro <chabbyabe@gmail.com>
2026-03-27 19:42:14 +0100

    Add user and ledger urls in urls.py

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
@@ -36,6 +36,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("", RedirectView.as_view(url="/redoc/", permanent=False)),
     path("admin/", admin.site.urls),
+    path("api/ledger/", include("ledger.urls")),
+    path("api/users/", include("users.urls")),
     # Swagger URLs
     path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),

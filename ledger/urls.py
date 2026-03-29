f9d2d57010b192f9d6e6d31afb27dcc07bbb0caf
Abegail Isidro <chabbyabe@gmail.com>
2026-03-29 21:54:39 +0200

    Create acounts url, serializers and views with test cases

--- ledger/urls.py
+++ ledger/urls.py
@@ -4,6 +4,7 @@ from .views.places import PlaceViewSet
 from .views.tags import TagViewSet
 from .views.stores import StoreViewSet
 from .views.categories import CategoryViewSet
+from .views.accounts import AccountViewSet
 
 urlpatterns = []
 
@@ -14,6 +15,7 @@ router.register(r'places', PlaceViewSet, basename='place')
 router.register(r'tags', TagViewSet, basename='tag')
 router.register(r'stores', StoreViewSet, basename='store')
 router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'accounts', AccountViewSet, basename='account')
 
 urlpatterns = [
     path('', include(router.urls)),

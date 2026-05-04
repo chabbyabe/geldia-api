b7701532cb783c21e07399a98e69348671b8803f
Abegail Isidro <chabbyabe@gmail.com>
2026-05-04 08:22:18 +0200

    Change pagination count from 20 to 50

--- core/pagination.py
+++ core/pagination.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 50
     page_size_query_param = "page_size"
     max_page_size = 100
 
